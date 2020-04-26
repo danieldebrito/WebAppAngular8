31b6c94013bd30d981b3829e69cc0b05c0fd17ee
unknown <danieldebrito@outlook.com>
2020-04-26 11:29:50 -0300

    secciones maquetado

---
+++ .history/src/app/components/seccions/dtoTecnico/apriete/apriete.component_20191206031548.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apriete',
+  templateUrl: './apriete.component.html',
+  styleUrls: ['./apriete.component.css']
+})
+export class AprieteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ .history/src/app/components/seccions/dtoTecnico/retenes-info/retenes-info.component_20191206031548.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-retenes-info',
+  templateUrl: './retenes-info.component.html',
+  styleUrls: ['./retenes-info.component.css']
+})
+export class RetenesInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
